f0203494a778ed3fe95721f9af8b0128cbac8aa2
Amin <mmdaminhosp@gmail.com>
2024-08-03 13:27:18 -0700

    change navabr design

--- src/components/header/Navbar/Navbar.js
+++ src/components/header/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import NavbarBottom from './NavbarDetails/NavbarBottom';
 
 function Navbar() {
   return (
-    <nav className='w-full h-full flex flex-col sticky top-0 z-50'>
+    <nav className='w-full h-full flex flex-col sticky top-0 z-50 max-sm:top-[-1px]'>
         <NavbarTop />
         <NavbarBottom />
     </nav>
